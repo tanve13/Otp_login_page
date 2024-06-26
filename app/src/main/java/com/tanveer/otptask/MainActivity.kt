47440297cd30bc6d649bc31c3ed34d40c027b7c2
tanve13 <tanusaini.jandir@gmail.com>
2024-06-26 14:15:48 +0530

    backspace_updated

--- app/src/main/java/com/tanveer/otptask/MainActivity.kt
+++ app/src/main/java/com/tanveer/otptask/MainActivity.kt
@@ -3,16 +3,35 @@ package com.tanveer.otptask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import com.tanveer.otptask.databinding.ActivityMainBinding
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
-    var binding: ActivityMainBinding? = null
-    var navController: NavController? = null
+    private var binding: ActivityMainBinding? = null
+   private var navController: NavController? = null
+    var appBarConfiguration : AppBarConfiguration? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-     }
+        navController = findNavController(R.id.host)
+        appBarConfiguration = navController?.graph?.let {
+            AppBarConfiguration(it)
+        }
+        setupActionBarWithNavController(navController!!,appBarConfiguration!!)
+        navController?.addOnDestinationChangedListener{ navController, destination, arguments ->
+            when(destination.id){
+                R.id.firstFragment -> supportActionBar?.title = resources.getString(R.string.first_fragment)
+                R.id.secondFragment -> supportActionBar?.title = resources.getString(R.string.second_fragment)
+                R.id.thirdFragment -> supportActionBar?.title = resources.getString(R.string.third_fragment)
+            }
+        }
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp()|| navController!!.popBackStack()
+    }
 }
